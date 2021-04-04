4ab618c1930b45b9f654ff9067a751d1e758ca69
Tolga Secme <tolgasecme@icloud.com>
2021-04-04 15:04:26 -0600

    Server connection is working

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const path = require('path');
+/* const path = require('path'); */
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+/* const exphbs = require('express-handlebars'); */
+/* const routes = require('./controllers'); */
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
+/* const helpers = require('./utils/helpers'); */
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,16 +23,16 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+/* const hbs = exphbs.create({ helpers }); */
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+/* app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars'); */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+/* app.use(express.static(path.join(__dirname, 'public'))); */
 
-app.use(routes);
+/* app.use(routes); */
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
