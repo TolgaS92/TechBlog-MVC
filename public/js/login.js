5c0a50541372d66bbc32d84fae4f926fd1e7fca1
Tolga Secme <tolgasecme@icloud.com>
2021-04-06 17:57:25 -0600

    Login and logout set up completed

--- public/js/login.js
+++ public/js/login.js
@@ -2,12 +2,14 @@ const loginForm = async (event) => {
      event.preventDefault();
      const name = document.querySelector('#username-login').value.trim();
      const password = document.querySelector('#password-login').value.trim();
+
      if (name && password) {
          const response = await fetch('/api/user/login', {
              method: 'POST',
              body: JSON.stringify({ name, password }),
              headers: { 'Content-Type': 'application/json' },
          });
+         
          if(response.ok) {
              document.location.replace('/');
          } else {alert('Failed to log in')};
@@ -18,12 +20,14 @@ const signupForm = async (event) => {
     event.preventDefault();
     const name = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+
     if (name && password) {
         const response = await fetch('/api/user', {
             method: 'POST',
             body: JSON.stringify({ name, password }),
             headers: { 'Content-Type': 'application/json' },
         });
+
         if(response.ok) {
             alert('Succesfully signed up!')
             document.location.replace('/');
