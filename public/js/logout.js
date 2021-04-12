fb18e9cdb4886a666f5602a69400968a84c99f53
Tolga Secme <tolgasecme@icloud.com>
2021-04-11 21:43:17 -0600

    Timer added to the logout js for 60 secs if the user not response they will be loggedout automatically

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,3 +1,36 @@
+let timer, currSeconds = 0;
+
+function resetTimer() {
+    /* Clear the previous interval */
+    clearInterval(timer);
+
+    /* Reset the seconds of the timer */
+    currSeconds = 0;
+
+    /* Set a new interval */
+    timer =
+    setInterval(startIdleTimer, 1000);
+}
+
+// Define the events that
+// would reset the timer
+window.onload = resetTimer;
+window.onmousemove = resetTimer;
+window.onmousedown = resetTimer;
+window.ontouchstart = resetTimer;
+window.onclick = resetTimer;
+window.onkeypress = resetTimer;
+
+function startIdleTimer() {
+    /* Increment the
+    timer seconds */
+    currSeconds++;
+    if (currSeconds > 60) {
+        logOut();
+    }
+}
+
+
 const logOut = async () => {
     const response = await fetch('/api/user/logout', {
         method: 'POST',
